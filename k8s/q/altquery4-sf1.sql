260ba0a6bb52305534ede62ac093e412f89d2c8e
George Chow <george@overcoil.com>
2022-02-10 13:32:18 -0800

    add queries for evaluating DPP

---
+++ k8s/q/altquery4-sf1.sql
@@ -0,0 +1,11 @@
+WITH
+  store_sales_sf1 AS (SELECT * FROM deltas3."$path$"."s3://tpc-datasets/tpcds-2.13/tpcds_sf1_delta/store_sales" ),
+  date_dim AS (SELECT * FROM deltas3."$path$"."s3://tpc-datasets/tpcds_1000_dat_delta/date_dim" )
+
+SELECT * FROM store_sales_sf1 store_sales
+
+JOIN date_dim ON ss_sold_date_sk = d_date_sk 
+
+WHERE d_year=1998 AND d_moy=7 AND d_dom=5
+;
+
